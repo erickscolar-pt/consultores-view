8fe1330c9063299d06b53c417e41fe80ba40269d
Erick Scolar <erick.scolar@outlook.com>
2024-06-16 23:14:24 -0300

    [main][Config tailwind]: [instalação e uso do tailwind]

---
+++ src/component/header/index.tsx
@@ -0,0 +1,17 @@
+export function Header() {
+  return (
+    <header className="w-full h-20 bg-blue-950">
+      <div>
+        <span>Project Sync</span>
+        <ul>
+          <li>
+            <button>Cadastrar cliente</button>
+          </li>
+          <li>
+            <button>Cadastrar projeto</button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { useContext, useState } from 'react';
 import { AuthContexts } from '../contexts/AuthContexts';
 import Head from 'next/head';
 import styles from '../../styles/styles.module.scss'
+import { Header } from '@/component/header';
 
 export default function Home() {
  
   return (
     <div>
+      <Header/>
       <h1>teste</h1>
     </div>
   );
